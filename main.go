f84562e5042403157cef85c8dee94083c6aac4bb
donnol <jdlau@126.com>
2016-09-30 15:35:08 +0800

    go爬虫

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	url := "http://www.mzitu.com/model"
+	crawl(url)
+}
+
+func crawl(url string) []string {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		panic(err)
+	}
+
+	images := []string{}
+	doc.Find(".postlist li").Each(func(i int, s *goquery.Selection) {
+		singleImage, ok := s.Find("img").Attr("data-original")
+		if !ok {
+			return
+		}
+		images = append(images, singleImage)
+
+		// 保存到文件
+		saveImage(singleImage)
+	})
+
+	return images
+}
+
+// 将图片下载并保存到本地
+func saveImage(url string) {
+	// 图片内容
+	res, err := http.Get(url)
+	defer res.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	// 目录
+	Dirname := "/donnol/Project/Tool/src/crawlmeizi/tmp/" + time.Now().Format("2006-01-02") + "/"
+	if !isDirExist(Dirname) {
+		os.Mkdir(Dirname, 0755)
+		fmt.Printf("dir %s created\n", Dirname)
+	}
+	// 文件
+	filename := filepath.Base(url)
+	dst, err := os.Create(Dirname + filename)
+	if err != nil {
+		panic(err)
+	}
+	// 写入文件
+	io.Copy(dst, res.Body)
+}
+
+func isDirExist(path string) bool {
+	p, err := os.Stat(path)
+	if err != nil {
+		return os.IsExist(err)
+	} else {
+		return p.IsDir()
+	}
+}
