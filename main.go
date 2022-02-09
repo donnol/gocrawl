8f338194ec6c5e3d1840becd8c8ebc24f0db5b23
donnol <donnol@outlook.com>
2022-02-09 18:42:43 +0800

    update

--- main.go
+++ main.go
@@ -3,17 +3,48 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+const (
+	link = "https://mmzztt.com/"
+)
+
+var (
+	chars = []string{
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+		"t", "u", "v", "w", "x", "y", "z",
+	}
+)
+
 func main() {
-	url := "https://mmzztt.com/"
-	crawl(url)
+	// crawl(link)
+	// saveImage("https://p.iimzt.com/2022/01/20s05h.jpg")
+	// https://p.iimzt.com/2022/01/20s02i.jpg
+
+	for i := 20; i < 30; i++ {
+		for j := 1; j < 20; j++ {
+			// for _, cb := range chars {
+			// 	_ = cb
+			for _, cf := range chars {
+
+				name := fmt.Sprintf("%02d%s%02d%s", i, "s", j, cf)
+				url := "https://p.iimzt.com/2022/01/" + name + ".jpg"
+				saveImage(url)
+
+				time.Sleep(250 * time.Millisecond)
+			}
+			// }
+		}
+	}
 }
 
 func crawl(url string) []string {
@@ -23,8 +54,9 @@ func crawl(url string) []string {
 	}
 
 	images := []string{}
-	doc.Find(".postlist li").Each(func(i int, s *goquery.Selection) {
-		singleImage, ok := s.Find("img").Attr("data-original")
+	doc.Find(".uk-grid li").Each(func(i int, s *goquery.Selection) {
+		singleImage, ok := s.Find("img").Attr("data-src")
+		fmt.Printf("image: %s\n", singleImage)
 		if !ok {
 			return
 		}
@@ -39,14 +71,29 @@ func crawl(url string) []string {
 
 // 将图片下载并保存到本地
 func saveImage(url string) {
-	// 图片内容
-	res, err := http.Get(url)
+	client := &http.Client{
+		Transport: http.DefaultTransport,
+		// CheckRedirect: func(*http.Request, []*http.Request) error { panic("not implemented") },
+		// Jar:           nil,
+		Timeout: 10 * time.Second,
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		panic(err)
+		log.Printf("new request failed: %+v\n", err)
+		return
+	}
+	req.Header.Add("Referer", link)
+	// 图片内容
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("client do failed: %+v\n", err)
+		return
 	}
 	defer res.Body.Close()
-	if err != nil {
-		panic(err)
+
+	if res.StatusCode == 404 || res.StatusCode == 429 {
+		fmt.Printf("http status code: %d, url: %s\n", res.StatusCode, url)
+		return
 	}
 
 	// 目录
@@ -54,16 +101,21 @@ func saveImage(url string) {
 	if !isDirExist(Dirname) {
 		err = os.MkdirAll(Dirname, 0755)
 		if err != nil {
-			panic(err)
+			log.Printf("mkdir all failed: %+v\n", err)
+			return
 		}
 		fmt.Printf("dir %s created\n", Dirname)
 	}
 
 	// 文件
 	filename := filepath.Base(url)
+	ext := filepath.Ext(filename)
+	n := rand.Int()
+	filename = strconv.Itoa(n) + ext
 	dst, err := os.Create(Dirname + filename)
 	if err != nil {
-		panic(err)
+		log.Printf("create file failed: %+v\n", err)
+		return
 	}
 
 	// 写入文件
